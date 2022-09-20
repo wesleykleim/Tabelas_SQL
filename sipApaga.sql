6e8129c85030e3cb368be52172fca061a75d74fc
wesleykleim <wesleykleim@gmail.com>
2022-09-20 15:48:17 -0300

    Apaga SIP

---
+++ sipApaga.sql
@@ -0,0 +1,28 @@
+-- Gerado por Oracle SQL Developer Data Modeler 19.4.0.350.1424
+--   em:        2022-06-22 11:49:18 BRT
+--   site:      Oracle Database 11g
+--   tipo:      Oracle Database 11g
+
+
+
+DROP TABLE t_sip_bairro CASCADE CONSTRAINTS;
+
+DROP TABLE t_sip_cidade CASCADE CONSTRAINTS;
+
+DROP TABLE t_sip_dependente CASCADE CONSTRAINTS;
+
+DROP TABLE t_sip_depto CASCADE CONSTRAINTS;
+
+DROP TABLE t_sip_endereco CASCADE CONSTRAINTS;
+
+DROP TABLE t_sip_estado CASCADE CONSTRAINTS;
+
+DROP TABLE t_sip_funcionario CASCADE CONSTRAINTS;
+
+DROP TABLE t_sip_funcionario_endereco CASCADE CONSTRAINTS;
+
+DROP TABLE t_sip_implantacao CASCADE CONSTRAINTS;
+
+DROP TABLE t_sip_projeto CASCADE CONSTRAINTS;
+
+DROP TABLE t_sip_tipo_endereco CASCADE CONSTRAINTS;
