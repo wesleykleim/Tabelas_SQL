d966f539b846e8e57a76ab68001dcbacb6a9c426
wesleykleim <wesleykleim@gmail.com>
2022-09-20 15:49:33 -0300

    APAGA RHSTU

---
+++ rhstuApaga.sql
@@ -0,0 +1,95 @@
+-- Gerado por Oracle SQL Developer Data Modeler 21.4.2.059.0838
+--   em:        2022-09-18 15:39:40 BRT
+--   site:      Oracle Database 11g
+--   tipo:      Oracle Database 11g
+
+
+
+DROP TABLE t_rhstu_bairro CASCADE CONSTRAINTS;
+
+DROP TABLE t_rhstu_cidade CASCADE CONSTRAINTS;
+
+DROP TABLE t_rhstu_consulta CASCADE CONSTRAINTS;
+
+DROP TABLE t_rhstu_consulta_forma_pagto CASCADE CONSTRAINTS;
+
+DROP TABLE t_rhstu_contato_paciente CASCADE CONSTRAINTS;
+
+DROP TABLE t_rhstu_email_paciente CASCADE CONSTRAINTS;
+
+DROP TABLE t_rhstu_endereco_paciente CASCADE CONSTRAINTS;
+
+DROP TABLE t_rhstu_estado CASCADE CONSTRAINTS;
+
+DROP TABLE t_rhstu_forma_pagamento CASCADE CONSTRAINTS;
+
+DROP TABLE t_rhstu_logradouro CASCADE CONSTRAINTS;
+
+DROP TABLE t_rhstu_medicamento CASCADE CONSTRAINTS;
+
+DROP TABLE t_rhstu_medico CASCADE CONSTRAINTS;
+
+DROP TABLE t_rhstu_paciente CASCADE CONSTRAINTS;
+
+DROP TABLE t_rhstu_paciente_plano_saude CASCADE CONSTRAINTS;
+
+DROP TABLE t_rhstu_plano_saude CASCADE CONSTRAINTS;
+
+DROP TABLE t_rhstu_prescicao_medica CASCADE CONSTRAINTS;
+
+DROP TABLE t_rhstu_telefone_paciente CASCADE CONSTRAINTS;
+
+DROP TABLE t_rhstu_tipo_contato CASCADE CONSTRAINTS;
+
+DROP TABLE t_rhstu_unid_hospitalar CASCADE CONSTRAINTS;
+
+-- predefined type, no DDL - MDSYS.SDO_GEOMETRY
+
+-- predefined type, no DDL - XMLTYPE
+
+
+
+
+
+-- Relatório do Resumo do Oracle SQL Developer Data Modeler: 
+-- 
+-- CREATE TABLE                            19
+-- CREATE INDEX                             0
+-- ALTER TABLE                             49
+-- CREATE VIEW                              0
+-- ALTER VIEW                               0
+-- CREATE PACKAGE                           0
+-- CREATE PACKAGE BODY                      0
+-- CREATE PROCEDURE                         0
+-- CREATE FUNCTION                          0
+-- CREATE TRIGGER                          16
+-- ALTER TRIGGER                            0
+-- CREATE COLLECTION TYPE                   0
+-- CREATE STRUCTURED TYPE                   0
+-- CREATE STRUCTURED TYPE BODY              0
+-- CREATE CLUSTER                           0
+-- CREATE CONTEXT                           0
+-- CREATE DATABASE                          0
+-- CREATE DIMENSION                         0
+-- CREATE DIRECTORY                         0
+-- CREATE DISK GROUP                        0
+-- CREATE ROLE                              0
+-- CREATE ROLLBACK SEGMENT                  0
+-- CREATE SEQUENCE                         16
+-- CREATE MATERIALIZED VIEW                 0
+-- CREATE MATERIALIZED VIEW LOG             0
+-- CREATE SYNONYM                           0
+-- CREATE TABLESPACE                        0
+-- CREATE USER                              0
+-- 
+-- DROP TABLESPACE                          0
+-- DROP DATABASE                            0
+-- 
+-- REDACTION POLICY                         0
+-- 
+-- ORDS DROP SCHEMA                         0
+-- ORDS ENABLE SCHEMA                       0
+-- ORDS ENABLE OBJECT                       0
+-- 
+-- ERRORS                                   0
+-- WARNINGS                                 0
